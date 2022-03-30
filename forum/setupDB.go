c8260116015d9f27d7e690bc1da471aed24042fe
pkdch <pau.darco@gmail.com>
2022-03-30 15:15:07 +0100

    Added 2 tables, session and post, tested post, datetime column not working, others are fine. session not tested yet

--- forum/setupDB.go
+++ forum/setupDB.go
@@ -2,7 +2,9 @@ package forum
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,11 +15,45 @@ func createSessionTable(db *sql.DB) {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
+
+	// test
+
 }
 func createUsersTable(db *sql.DB) {
 
 }
 func createPostsTable(db *sql.DB) {
+	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS post (postID INTEGER PRIMARY KEY, title TEXT, content TEXT,	datetime DATETIME);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	stmt.Exec()
+
+	// test insert
+	stmt, err = db.Prepare("INSERT INTO post (postID, title, content, datetime) VALUES (?,?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	stmt.Exec(0, "test0", "testing0", 0)
+
+	// test query
+	var pID int
+	var title string
+	var content string
+	var datetime time.Time
+
+	rows, err := db.Query("SELECT * FROM post")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		rows.Scan(&pID, &title, &content, &datetime)
+		fmt.Printf("Post: %d, title: %s, content: %s, at %v\n", pID, title, content, datetime)
+	}
 
 }
 func InitDB() {
